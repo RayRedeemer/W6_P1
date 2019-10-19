90eb18f4a49ad3ea06956b67e6ccd24264ac3956
Abdulshaheed Alqunber <asq@bu.edu>
2019-10-19 01:10:19 -0400

    done, it was hard getting the app to build

--- Lect7_FragmentTransactions_sse/app/src/main/java/com/example/sse/fragmenttransactions_sse/MainActivity.java
+++ Lect7_FragmentTransactions_sse/app/src/main/java/com/example/sse/fragmenttransactions_sse/MainActivity.java
@@ -6,7 +6,7 @@ package com.example.sse.fragmenttransactions_sse;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +20,11 @@ public class MainActivity extends AppCompatActivity{
     //
     //1. Just include them in the Activity's layout.
     //
-    //2. Instantatiate and work with them in code.
+    //2. Instantiate and work with them in code.
     // in code you have much more control.
 
     //3. create objects to reference the views, including fragments.
-private
+    private
     Frag_One  f1;
     Frag_Two  f2;
     Frag_Three  f3;
@@ -43,7 +43,7 @@ private
         setContentView(R.layout.activity_main);
 
 
-    //4. get references for our views.
+        //4. get references for our views.
 
         btnFrag1 = (Button) findViewById(R.id.btnFrag1);
         btnFrag2 = (Button) findViewById(R.id.btnFrag2);
@@ -52,28 +52,28 @@ private
 
 //        f1 = (Frag_One) findViewById(R.id.frag1);  //Q: Why won't this work for fragments?  Does the fragment even exist in R.java? _____________
 
-    //5a.  We actually have to create the fragments ourselves.  We left R behind when we took control of rendering.
+        //5a.  We actually have to create the fragments ourselves.  We left R behind when we took control of rendering.
         f1 = new Frag_One();
         f2 = new Frag_Two();
         f3 = new Frag_Three();
 
-    //5b. Grab a reference to the Activity's Fragment Manager, Every Activity has one!
-       fm = getFragmentManager ();  //that was easy.
+        //5b. Grab a reference to the Activity's Fragment Manager, Every Activity has one!
+        fm = getFragmentManager ();  //that was easy.
 //         fm = getSupportFragmentManager();  // **When would you use this instead?? A: __________________
 
 
-    //5c. Now we can "plop" fragment(s) into our container.
+        //5c. Now we can "plop" fragment(s) into our container.
         FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
         ft.add(R.id.FragLayout, f1, "tag1");  //now we have added our fragement to our Activity programmatically.  The other fragments exist, but have not been added yet.
         ft.addToBackStack ("myFrag1");  //why do we do this?
         ft.commit ();  //don't forget to commit your changes.  It is a transaction after all.
 
-    btnFrag1.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            showFrag1();
-        }
-    });
+        btnFrag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFrag1();
+            }
+        });
 
         btnFrag2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,20 +91,26 @@ private
 
     }
 
-public void showFrag1() {
-    f1 = (Frag_One) fm.findFragmentByTag("tag1");   //what should we do if f1 doesn't exist anymore?  How do we check and how do we fix?
-    FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
+    public void showFrag1() {
 
-    ft.hide(f2);
-    ft.hide(f3);
-    ft.show(f1);   //why does this not *always* crash?
-    ft.commit();
-}
+        if (f1 == null)
+            f1 = new Frag_One();
+
+        f1 = (Frag_One) fm.findFragmentByTag("tag1");   //what should we do if f1 doesn't exist anymore?  How do we check and how do we fix?
+        FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
+
+        ft.replace(R.id.FragLayout, f1);
+        ft.addToBackStack("myFrag1");
+//    ft.hide(f2);
+//    ft.hide(f3);
+//    ft.show(f1);   //why does this not *always* crash?
+        ft.commit();
+    }
 
     public void showFrag2() {
 
         if (f2 == null)
-          f2 = new Frag_Two();
+            f2 = new Frag_Two();
 
         FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction and start the transaction.
         ft.replace(R.id.FragLayout, f2);
@@ -115,10 +121,15 @@ public void showFrag1() {
 
     public void showFrag3() {
 
+        if (f3 == null)
+            f3 = new Frag_Three();
+
         FragmentTransaction ft = fm.beginTransaction ();  //Create a reference to a fragment transaction.
-        ft.detach(f1);   //what would happen if f1, f2, or f3 were null?  how would we check and fix this?
-        ft.detach(f2);
-        ft.attach(f3);
+        ft.replace(R.id.FragLayout, f3);
+        ft.addToBackStack("myFrag3");
+//        ft.detach(f1);   //what would happen if f1, f2, or f3 were null?  how would we check and fix this?
+//        ft.detach(f2);
+//        ft.attach(f3);
         ft.commit();
     }
 }
